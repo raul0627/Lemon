31386d3f98cef1f32785cb5f31428124cd81e19b
raul0627 <supertommail-98@yahoo.com>
2016-06-21 15:39:39 +0800

    upAndDown-bigFile upload

---
+++ src/main/java/com/hexin/Dto/FileTrans.java
@@ -0,0 +1,34 @@
+package com.hexin.Dto;
+
+/**
+ * Created by Administrator on 2016/1/22.
+ */
+public class FileTrans {
+    private int fileFlag;
+    private int dataLength;
+    private String fileName;
+
+    public int getFileFlag() {
+        return fileFlag;
+    }
+
+    public void setFileFlag(int fileFlag) {
+        this.fileFlag = fileFlag;
+    }
+
+    public int getDataLength() {
+        return dataLength;
+    }
+
+    public void setDataLength(int dataLength) {
+        this.dataLength = dataLength;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
